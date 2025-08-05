e9b1821e1a39f2763cb11fc34ce358ce97a832e5
vshe0007 <vshe0007@student.monash.edu>
2025-08-05 21:57:25 +1000

    Added all the activities performed in Studio Week 1.

---
+++ Studio_Week1/src/components/MyName.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+
+})
+</script>
+
+
+
+<template>
+    <div class="greetings">
+        <h1 class="green">{{ name }}</h1>
+        <h3>
+            This is my first Component.
+        </h3>
+    </div>
+</template>
+
+
+
+<style scoped>
+h3 {
+    color: #FFFF00;
+}
+</style>
